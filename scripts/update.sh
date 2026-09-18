4c0d1d855ff3b182bedf401080b2f679aead2180
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2026-09-18 22:44:52 +0100

    updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,7 @@ set -e
 
 # mise's rc-file activation doesn't run in non-interactive SSH sessions,
 # so load it explicitly here.
+#
 export PATH="$HOME/.local/bin:$PATH"
 eval "$(mise activate bash)"
 
